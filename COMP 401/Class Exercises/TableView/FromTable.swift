20a8d946547b36309db7dc1b838a88ddaecdf81a
Oliver Spryn <wot200@gmail.com>
2015-02-20 23:38:43 -0500

    Added TabBar and TableView class projects to iOS class.

---
+++ COMP 401/Class Exercises/TableView/FromTable.swift
@@ -0,0 +1,43 @@
+//
+//  FromTable.swift
+//  TableView
+//
+//  Created by Oliver Spryn on 2/19/15.
+//  Copyright (c) 2015 Oliver Spryn. All rights reserved.
+//
+
+import UIKit
+
+class FromTable: UIViewController {
+
+    @IBOutlet weak var Back: UIButton!
+    @IBOutlet weak var Label: UILabel!
+    var Delegate: ITable!
+    var Text = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Label.text = Text
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Back(sender: UIButton) {
+        Delegate.done(self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ COMP 401/Class Exercises/TableView/TableView/ITable.swift
@@ -0,0 +1,13 @@
+//
+//  ITable.swift
+//  TableView
+//
+//  Created by Oliver Spryn on 2/20/15.
+//  Copyright (c) 2015 Oliver Spryn. All rights reserved.
+//
+
+import UIKit
+
+protocol ITable {
+   func done(child: FromTable)
+}
